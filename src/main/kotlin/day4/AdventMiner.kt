6144cf089a660e04225768b33ba342babd9c9c16
Nadav Cohen <nadavcoh@gmail.com>
2016-08-17 21:51:04 -0700

    day4 completed

--- src/main/kotlin/day4/AdventMiner.kt
+++ src/main/kotlin/day4/AdventMiner.kt
@@ -1,9 +1,19 @@
 package day4
 
+import org.apache.commons.codec.digest.DigestUtils
+
 class AdventMiner {
 
-    fun findFirstMd5With5LeadingZeros(code: String): Int {
-        return 100
+    fun findFirstMd5With5LeadingZeros(code: String, leadingZeros: Int = 5): Int {
+        val s = StringBuilder(leadingZeros)
+        repeat(leadingZeros) { s.append("0") }
+
+        for (i in 1..5000000) {
+            val digest = DigestUtils.md5Hex(code + i.toString())
+            if (digest.startsWith(s))
+                return i
+        }
+        return -1
     }
 
 }
--- src/test/groovy/day4/AdventMinerSpec.groovy
+++ src/test/groovy/day4/AdventMinerSpec.groovy
@@ -9,17 +9,19 @@ class AdventMinerSpec extends Specification {
 
     AdventMiner miner = new AdventMiner()
 
-    def 'code #code should return #expected'(code, expected) {
+    def 'code #code should return #expected'(code, zeros, expected) {
         when:
-        def result = miner.findFirstMd5With5LeadingZeros(code)
+        def result = miner.findFirstMd5With5LeadingZeros(code, zeros)
 
         then:
         result == expected
 
         where:
-        code      | expected
-        'abcdef'  | 609043
-        'pqrstuv' | 1048970
+        code       | zeros | expected
+        'abcdef'   | 5     | 609043
+        'pqrstuv'  | 5     | 1048970
+        'ckczppom' | 5     | 117946
+        'ckczppom' | 6     | 3938038
     }
 
 }
